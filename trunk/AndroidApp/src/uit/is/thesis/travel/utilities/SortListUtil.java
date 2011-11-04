5adea1c34546c5900e1b077af5e9d8f12685c266
lehi3u1989@gmail.com <lehi3u1989@gmail.com@3bfb1385-3220-3565-0149-dbb0b676ecea>
2011-11-04 08:03:36 +0000

    04.11.2011 Sort List by name, distance, rating

--- trunk/AndroidApp/src/uit/is/thesis/travel/utilities/SortListUtil.java
+++ trunk/AndroidApp/src/uit/is/thesis/travel/utilities/SortListUtil.java
@@ -3,10 +3,10 @@
  */
 package uit.is.thesis.travel.utilities;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
-
-import uit.is.thesis.travel.models.*;
-
+import uit.is.thesis.travel.models.PlaceModel;
 
 /**
  * @author LEHIEU
@@ -14,30 +14,69 @@ import uit.is.thesis.travel.models.*;
  */
 public class SortListUtil {
 	
-	/*public static List<PlaceModel> SortListByName(List<PlaceModel> original)
-	{
-		List<PlaceModel> afterSorted;
-		
-		return afterSorted;
-		
+	public static List<PlaceModel> SortListByName(List<PlaceModel> original)
+	{	
+		Collections.sort(original, new NameComparator());
+		return original;	
 	}
 	
 	public static List<PlaceModel> SortListByDistance(List<PlaceModel> original)
 	{
-		List<PlaceModel> afterSorted;
-		
-		return afterSorted;
-		
+		Collections.sort(original, new DistanceComparator());
+		return original;			
 	}
 	
 	public static List<PlaceModel> SortListByRating(List<PlaceModel> original)
 	{
-		List<PlaceModel> afterSorted;
-		
-		return afterSorted;
-		
-	}*/
+		Collections.sort(original, new RatingComparator());
+		return original;
+	}
 
+}
 
+//Sort data by name
+class NameComparator implements Comparator<PlaceModel> {
 
+	public int compare(PlaceModel object1, PlaceModel object2) {
+		// TODO Auto-generated method stub
+		String name1 = object1.getName();
+		String name2 = object2.getName();		
+		return name1.compareTo(name2);
+	}
+}
+
+// Sort data by distance
+class DistanceComparator implements Comparator<PlaceModel> {
+
+	public int compare(PlaceModel object1, PlaceModel object2) {
+		// TODO Auto-generated method stub
+		double dis1 = object1.getDistance();
+		double dis2 = object2.getDistance();	
+		if(dis1 > dis2)
+			return 1;
+		else {
+			if(dis1 < dis2)
+				return -1;
+			else
+				return 0;
+		}		
+	}	
+}
+
+//Sort data by Rating
+class RatingComparator implements Comparator<PlaceModel> {
+
+	public int compare(PlaceModel object1, PlaceModel object2) {
+		// TODO Auto-generated method stub
+		double rat1 = object1.getGeneral_rating();
+		double rat2 = object2.getGeneral_rating();	
+		if(rat1 > rat2)
+			return -1;
+		else {
+			if(rat1 < rat2)
+				return 1;
+			else
+				return 0;
+		}		
+	}	
 }
