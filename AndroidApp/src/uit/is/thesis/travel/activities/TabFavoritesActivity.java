7864bb14ea2e176d74cb286b85c15043adc69712
lehi3u1989 <lehi3u1989@gmail.com>
2011-11-11 10:14:59 +0000

    11.11.2011 17h15 Finish Tab Context

--- AndroidApp/src/uit/is/thesis/travel/activities/TabFavoritesActivity.java
+++ AndroidApp/src/uit/is/thesis/travel/activities/TabFavoritesActivity.java
@@ -23,7 +23,6 @@ import android.database.Cursor;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
