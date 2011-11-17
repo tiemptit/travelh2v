ea63bc9d58d57b1af090517787ade025b7aa1380
lehi3u1989 <lehi3u1989@gmail.com>
2011-11-17 01:38:00 +0000

    17.11.2011 Finish Rating Service

--- RecommenderSystem/TravelWebService/TravelWebService/RatingModel.cs
+++ RecommenderSystem/TravelWebService/TravelWebService/RatingModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data;
 using RecommenderSystem.Core.Helper;
 
 namespace TravelWebService
@@ -23,8 +24,51 @@ namespace TravelWebService
         // insert a rate into DB
         public bool Rate()
         {
-            //return DbHelper.RunScripts("insert into real_ratings values ('',id_user,id_place,id_temperature,id_companion,id_farmiliarity,id_mood,id_budget,id_budget,id_weather,id_travel_length,time,rating)");
-            return false;
+            // if user not register, auto register with the email
+            int id_user = checkIfUserExisted();
+            if (id_user == -1)
+            {
+                DbHelper.RunScripts(string.Format("insert into users values ('" + username + "','123',1989,1)"));
+                id_user = checkIfUserExisted();
+                return DbHelper.RunScripts(string.Format("insert into real_ratings values (" + id_user + "," + id_place + "," + id_temperature + "," + id_companion + "," + id_farmiliarity + "," + id_mood + "," + id_budget + "," + id_weather + "," + id_travel_length + "," + time + "," + rating + ")"));
+            }
+            else 
+            { 
+                //if user rated before, update
+                int id_real_rating = checkIfRatingExisted();
+                if (id_real_rating != -1) 
+                {
+                    return DbHelper.RunScripts(string.Format("update real_ratings set rating = " + rating + " where id = " + id_real_rating));
+                } // else, insert new rating
+                else 
+                {
+                    return DbHelper.RunScripts(string.Format("insert into real_ratings values ("+id_user+","+id_place+","+id_temperature+","+id_companion+","+id_farmiliarity+","+id_mood+","+id_budget+","+id_weather+","+id_travel_length+","+time+","+rating+")"));
+                } 
+            }        
+        }
+
+        public int checkIfUserExisted()
+        {
+            DataTable dt = DbHelper.RunScriptsWithTable(string.Format("select id from users where email ='" + username + "'"));
+            if (dt.Rows.Count != 0)
+            {
+                return Convert.ToInt32(dt.Rows[0][0]);
+            }
+            else
+                return -1;
+        }
+
+        public int checkIfRatingExisted()
+        {
+            int id_user = checkIfUserExisted();
+
+            DataTable dt = DbHelper.RunScriptsWithTable(string.Format("select id from real_ratings where id_user=" + id_user + " and id_place = " + id_place + " and id_temperature = " + id_temperature + " and id_companion = " + id_companion + " and id_farmiliarity = " + id_farmiliarity + " and id_mood = " + id_mood + " and id_budget = " + id_budget + " and id_weather = " + id_weather + " and id_travel_length = " + id_travel_length));
+            if (dt.Rows.Count != 0)
+            {
+                return Convert.ToInt32(dt.Rows[0][0]);
+            }
+            else
+                return -1;
         }
     }
 }
