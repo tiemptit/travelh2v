6cf75442e69def883a16317097a5609708b50430
lehi3u1989@gmail.com <lehi3u1989@gmail.com@3bfb1385-3220-3565-0149-dbb0b676ecea>
2011-11-26 15:07:22 +0000

    26.11.2011: Finish tab Profile + Context (add DatetimePicker)

--- trunk/AndroidApp/src/uit/is/thesis/travel/activities/TabContextActivity.java
+++ trunk/AndroidApp/src/uit/is/thesis/travel/activities/TabContextActivity.java
@@ -10,6 +10,9 @@ package uit.is.thesis.travel.activities;
 import java.util.Date;
 import uit.is.thesis.travel.SQLiteHelper.SQLiteDBAdapter;
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.text.format.DateFormat;
@@ -17,7 +20,9 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.Spinner;
+import android.widget.TimePicker;
 
 public class TabContextActivity extends Activity implements OnClickListener {
 	SQLiteDBAdapter mDBAdapter = null;
@@ -25,6 +30,9 @@ public class TabContextActivity extends Activity implements OnClickListener {
 			spinner_mood, spinner_familiarity, spinner_budget,
 			spinner_travel_length;
 	Button btnSaveC, btnResetC, btnPickDate, btnPickTime;
+	int mYear, mMonth, mDay, mHour, mMinute;
+	static final int DATE_DIALOG_ID = 0;
+	static final int TIME_DIALOG_ID = 1;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -47,7 +55,7 @@ public class TabContextActivity extends Activity implements OnClickListener {
 		btnPickDate.setOnClickListener(this);
 		btnPickTime = (Button) findViewById(R.id.btnPickTime);
 		btnPickTime.setOnClickListener(this);
-		
+
 		if (this.mDBAdapter == null) {
 			this.mDBAdapter = new SQLiteDBAdapter(this);
 			mDBAdapter.open();
@@ -99,7 +107,6 @@ public class TabContextActivity extends Activity implements OnClickListener {
 
 		// load current context config
 		LoadCurrentContextConfig();
-
 	}
 
 	// button onclick handler
@@ -108,11 +115,13 @@ public class TabContextActivity extends Activity implements OnClickListener {
 		switch (v.getId()) {
 		case R.id.btnResetC: {
 			try {
-				//get current date time
-				Date d = new Date(); 
-				String s_plit[] = DateFormat.format("yyyy-MM-dd hh:mm:ss", d.getTime()).toString().split(" ");
-				mDBAdapter.updateContextConfig(0, 0, 0, 0, 0, 0, 0,
-						s_plit[0]+"and"+s_plit[1]);
+				// get current date time
+				Date d = new Date();
+				String s_plit[] = DateFormat
+						.format("yyyy-MM-dd hh:mm", d.getTime()).toString()
+						.split(" ");
+				mDBAdapter.updateContextConfig(0, 0, 0, 0, 0, 0, 0, s_plit[0]
+						+ "and" + s_plit[1]);
 				LoadCurrentContextConfig();
 			} catch (Exception e) {
 			}
@@ -134,38 +143,123 @@ public class TabContextActivity extends Activity implements OnClickListener {
 			}
 		}
 			break;
+		case R.id.btnPickDate: {
+			try {
+				showDialog(DATE_DIALOG_ID);
+			} catch (Exception e) {
+			}
+		}
+			break;
+		case R.id.btnPickTime: {
+			try {
+				showDialog(TIME_DIALOG_ID);
+			} catch (Exception e) {
+			}
+		}
+			break;
 		}
 	}
-	
-	public void LoadCurrentContextConfig(){
+
+	public void LoadCurrentContextConfig() {
 		Cursor c = mDBAdapter.getContextConfig();
 		startManagingCursor(c);
 		int value; // context value
 		c.moveToPosition(0); // set current Temperature
-		value = Integer.parseInt(c.getString(c.getColumnIndex("current_value")));
+		value = Integer
+				.parseInt(c.getString(c.getColumnIndex("current_value")));
 		spinner_temperature.setSelection(value);
 		c.moveToPosition(1); // set current Weather
-		value = Integer.parseInt(c.getString(c.getColumnIndex("current_value")));
+		value = Integer
+				.parseInt(c.getString(c.getColumnIndex("current_value")));
 		spinner_weather.setSelection(value);
 		c.moveToPosition(2); // set current Companion
-		value = Integer.parseInt(c.getString(c.getColumnIndex("current_value")));
+		value = Integer
+				.parseInt(c.getString(c.getColumnIndex("current_value")));
 		spinner_companion.setSelection(value);
 		c.moveToPosition(3); // set current Mood
-		value = Integer.parseInt(c.getString(c.getColumnIndex("current_value")));
+		value = Integer
+				.parseInt(c.getString(c.getColumnIndex("current_value")));
 		spinner_mood.setSelection(value);
 		c.moveToPosition(4); // set current Familiarity
-		value = Integer.parseInt(c.getString(c.getColumnIndex("current_value")));
+		value = Integer
+				.parseInt(c.getString(c.getColumnIndex("current_value")));
 		spinner_familiarity.setSelection(value);
 		c.moveToPosition(5); // set current Budget
-		value = Integer.parseInt(c.getString(c.getColumnIndex("current_value")));
+		value = Integer
+				.parseInt(c.getString(c.getColumnIndex("current_value")));
 		spinner_budget.setSelection(value);
 		c.moveToPosition(6); // set current Travel length
-		value = Integer.parseInt(c.getString(c.getColumnIndex("current_value")));
+		value = Integer
+				.parseInt(c.getString(c.getColumnIndex("current_value")));
 		spinner_travel_length.setSelection(value);
 		c.moveToPosition(7); // set current Time
 		String time = c.getString(c.getColumnIndex("current_value"));
-		String[] time_plit = time.split("and");
-		btnPickDate.setText(time_plit[0]);
-		btnPickTime.setText(time_plit[1]);		
+		String[] datetime_plit = time.split("and");
+		btnPickDate.setText(datetime_plit[0]);
+		btnPickTime.setText(datetime_plit[1]);
+		String[] date_plit = datetime_plit[0].split("-");
+		mYear = Integer.parseInt(date_plit[0].trim());
+		mMonth = Integer.parseInt(date_plit[1].trim());
+		mDay = Integer.parseInt(date_plit[2].trim());
+		String[] time_plit = datetime_plit[1].split(":");
+		mHour = Integer.parseInt(time_plit[0].trim());
+		mMinute = Integer.parseInt(time_plit[1].trim());
 	}
+
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		switch (id) {
+		case DATE_DIALOG_ID: {
+			return new DatePickerDialog(this, mDateSetListener, mYear,
+					mMonth - 1, mDay);
+		}
+		case TIME_DIALOG_ID: {
+			return new TimePickerDialog(this, mTimeSetListener, mHour, mMinute,
+					true);
+		}
+		}
+		return null;
+	}
+
+	// updates the date in the button Date
+	private void updateDisplayDate() {
+		btnPickDate.setText(new StringBuilder()
+				// Month is 0 based so add 1
+				.append(mYear).append("-").append(mMonth).append("-")
+				.append(mDay).append(" "));
+	}
+	
+	// the callback received when the user set the date in the dialog
+	private DatePickerDialog.OnDateSetListener mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+		public void onDateSet(DatePicker view, int year, int monthOfYear,
+				int dayOfMonth) {
+			mYear = year;
+			mMonth = monthOfYear + 1;
+			mDay = dayOfMonth;
+			updateDisplayDate();
+		}
+	};
+
+	// updates the time we display in the TextView
+	private void updateDisplayTime() {
+		btnPickTime.setText(new StringBuilder().append(pad(mHour)).append(":")
+				.append(pad(mMinute)));
+	}
+	
+	// format 2-number for time display
+	private static String pad(int c) {
+		if (c >= 10)
+			return String.valueOf(c);
+		else
+			return "0" + String.valueOf(c);
+	}
+
+	// the callback received when the user set the time in the dialog
+	private TimePickerDialog.OnTimeSetListener mTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+		public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+			mHour = hourOfDay;
+			mMinute = minute;
+			updateDisplayTime();
+		}
+	};
 }
