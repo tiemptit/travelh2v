6395c0b5f0e3b22553e34e181e47237c67cef3ac
lehi3u1989 <lehi3u1989@gmail.com>
2011-12-29 09:04:42 +0000

    29.12.2011 Rate (4 ngu canh)

--- RecommenderSystem/TravelWebService/TravelWebService/RatingModel.cs
+++ RecommenderSystem/TravelWebService/TravelWebService/RatingModel.cs
@@ -11,13 +11,9 @@ namespace TravelWebService
     {
         public string username { get; set; }
         public int id_place { get; set; }
-        public int id_temperature { get; set; }
         public int id_companion { get; set; }
-        public int id_farmiliarity { get; set; }
-        public int id_mood { get; set; }
         public int id_budget { get; set; }
         public int id_weather { get; set; }
-        public int id_travel_length { get; set; }
         public string time { get; set; }
         public float rating { get; set; }
 
@@ -28,9 +24,9 @@ namespace TravelWebService
             int id_user = checkIfUserExisted();
             if (id_user == -1)
             {
-                DbHelper.RunScripts(string.Format("insert into users values ('" + username + "','123',1989,1)"), "Connection String");
+                DbHelper.RunScripts(string.Format("insert into users values ('" + username + "','1989-03-01',1)"), "Connection String");
                 id_user = checkIfUserExisted();
-                return DbHelper.RunScripts(string.Format("insert into real_ratings values (" + id_user + "," + id_place + "," + id_temperature + "," + id_companion + "," + id_farmiliarity + "," + id_mood + "," + id_budget + "," + id_weather + "," + id_travel_length + "," + time + "," + rating + ")"), "Connection String");
+                return DbHelper.RunScripts(string.Format("insert into real_ratings values (" + id_user + "," + id_place + "," + id_companion + "," + id_budget + "," + id_weather + ",'" + time + "'," + rating + ")"), "Connection String");
             }
             else 
             { 
@@ -42,7 +38,7 @@ namespace TravelWebService
                 } // else, insert new rating
                 else 
                 {
-                    return DbHelper.RunScripts(string.Format("insert into real_ratings values (" + id_user + "," + id_place + "," + id_temperature + "," + id_companion + "," + id_farmiliarity + "," + id_mood + "," + id_budget + "," + id_weather + "," + id_travel_length + "," + time + "," + rating + ")"), "Connection String");
+                    return DbHelper.RunScripts(string.Format("insert into real_ratings values (" + id_user + "," + id_place + "," + id_companion + "," + id_budget + "," + id_weather + ",'" + time + "'," + rating + ")"), "Connection String");
                 } 
             }        
         }
@@ -62,7 +58,7 @@ namespace TravelWebService
         {
             int id_user = checkIfUserExisted();
 
-            DataTable dt = DbHelper.RunScriptsWithTable(string.Format("select id from real_ratings where id_user=" + id_user + " and id_place = " + id_place + " and id_temperature = " + id_temperature + " and id_companion = " + id_companion + " and id_farmiliarity = " + id_farmiliarity + " and id_mood = " + id_mood + " and id_budget = " + id_budget + " and id_weather = " + id_weather + " and id_travel_length = " + id_travel_length), "Connection String");
+            DataTable dt = DbHelper.RunScriptsWithTable(string.Format("select id from real_ratings where id_user=" + id_user + " and id_place = " + id_place + " and id_companion = " + id_companion +  " and id_budget = " + id_budget + " and id_weather = " + id_weather + " and time = '" + time +"'"), "Connection String");
             if (dt.Rows.Count != 0)
             {
                 return Convert.ToInt32(dt.Rows[0][0]);
